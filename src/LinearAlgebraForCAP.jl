0bbec5fb1f0ec366c2a3377bc3880e6d2f19f5ba
Fabian Zickgraf <f.zickgraf@dashdos.com>
2024-08-19 11:53:27 +0200

    Import all operations in Base and in CAP packages by default

--- src/LinearAlgebraForCAP.jl
+++ src/LinearAlgebraForCAP.jl
@@ -2,16 +2,9 @@ module LinearAlgebraForCAP
 
 @nospecialize
 
-import Base./
-
 using MatricesForHomalg
 
 using CAP
-
-import CAP.StringGAP_OPERATION
-import CAP.ViewString
-import CAP.DisplayString
-import CAP.LaTeXOutput
 
 using MonoidalCategories
 
@@ -81,9 +74,7 @@ end
 
 include("init.jl")
 
-const IsZero = CAP.IsZero # MatricesForHomalg also exports IsZero
-const IsOne = CAP.IsOne # MatricesForHomalg also exports IsOne
-
+# IsZero/IsOne: install the functions in MatricesForHomalg as methods for the attributes defined in CAP
 @InstallMethod( IsZero, [ IsHomalgMatrix ], M -> MatricesForHomalg.IsZero( M ) );
 @InstallMethod( IsOne, [ IsHomalgMatrix ], M -> MatricesForHomalg.IsOne( M ) );
 
